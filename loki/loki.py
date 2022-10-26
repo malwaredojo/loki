f5edf52d396e898a26cead06006c6d305d8cc9d5
Malware Dojo <115943414+malwaredojo@users.noreply.github.com>
2022-10-26 12:19:54 +0530

    Update loki.py

--- loki/loki.py
+++ loki/loki.py
@@ -14,8 +14,6 @@ def args_func():
     parser.add_argument('-s','--simple',help='Simple Information Generator', dest='simple', action='store_true')
     parser.add_argument('-sp','--simplewithpic',help='Simple Information Generation with Pic', dest='simplewithpic', action='store_true')
     parser.add_argument('-p','--profession',help='Specify the Profession beforehand', dest='profession')
-    parser.add_argument('-soc','--social-create',help='Simple Information Generation with Pic & Create Profiles on Facebook, Instagram, TikTok', dest='socialmedia', action='store_true')
-    parser.add_argument('-g','--gender',choices=['male', 'female'], help='Specify the gender of the sock puppet', dest='gender')
     return parser.parse_args()
 
 def banner():
@@ -44,11 +42,7 @@ def main():
     args = args_func()
     
     if args.simple == True:
-        if args.gender == 'male':
-            print('Male')
-        elif args.gender == 'female':
-            print('Female')
-        elif args.profession is not None:
+        if args.profession is not None:
             defined_profession(args.profession[0])
         else:
             simple()
