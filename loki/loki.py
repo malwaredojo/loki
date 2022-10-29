0eb837448e12b6022d741e2fdc1319da5e427743
Malware Dojo <115943414+malwaredojo@users.noreply.github.com>
2022-10-29 18:22:58 +0530

    Update loki.py

--- loki/loki.py
+++ loki/loki.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 from core.colours import green, end, bad, yellow, white, sarcastic
-from core.simple import * #simpleinfogather, simpleinfogathermalewithprofession, simplewithprofession
+from core.simple import * 
 from core.simplewithpic import maininfogather
-from core.test import simpleinfogathermale, simpleinfogatherfemale
 
 
 version = '0.0.1'
